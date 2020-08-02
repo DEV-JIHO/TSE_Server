567649fb1a76924fc1580fc9d0f3d312e6c0764d
Jeong <wjdwlgh_34@naver.com>
2020-08-02 15:31:42 +0900

    fastapi docker init

--- app/main.py
+++ app/main.py
@@ -1,43 +1,15 @@
-from typing import List
+from typing import Optional
 
-from fastapi import Depends, FastAPI, HTTPException, Request
-from sqlalchemy.orm import Session
-
-from . import crud, models, schemas
-from .database import SessionLocal, engine
-
-models.Base.metadata.create_all(bind=engine)
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-# 미세먼지 데이터 관련 함수 추가
-# 주고 받을 정보에 따라 추가 예정
-
-
-@app.post("/creat_dustdata/")
-def create_dust(dust: schemas.dustCreate, db: Session = Depends(get_db)):
-    return crud.create_dust(db=db, dust=dust)
-
-
-# 위치 정보에 따라서 값을 반환
-# 위치를 좌표로 받기 때문에 이 부분을 어떻게 할 것인가 상의
-@app.get("/dust/all", response_model=List[schemas.Dust])
-async def read_dusts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    dusts = crud.get_dusts(db, skip=skip, limit=limit)
-    return dusts
-
-
-@app.get("/get_dustdata/{dust_id}", response_model=schemas.Dust)
-async def read_dusts(dust_id: int = 0, db: Session = Depends(get_db)):
-    dusts = crud.get_dustId(db, dust_id=dust_id)
-    return dusts
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
